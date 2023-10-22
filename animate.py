a024a37ff6189ba3f8c69a5a2421b29487648746
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2023-10-22 15:05:45 -0400

    Add files via upload

--- animate.py
+++ animate.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import matplotlib.pyplot as plt, json
 import numpy as np
 
@@ -14,36 +12,4 @@ while True:
     plt.plot(np.arange(len(best_overtime)) + 1, best_overtime)
     plt.draw()
     plt.pause(0.0001)
-=======
-import matplotlib.pyplot as plt, json
-import numpy as np
-
-plt.ion()
-    
-while True:
-    try:
-        best_overtime = np.array(json.load(open('generations.json', 'r+')))
-    except:
-        continue
-    
-    plt.plot(np.arange(len(best_overtime)) + 1, best_overtime)
-    plt.draw()
-    plt.pause(0.0001)
->>>>>>> 72debf778cab3d43425f2606e0f1fdbac5082207
-=======
-import matplotlib.pyplot as plt, json
-import numpy as np
-
-plt.ion()
-    
-while True:
-    try:
-        best_overtime = np.array(json.load(open('generations.json', 'r+')))
-    except:
-        continue
-    
-    plt.plot(np.arange(len(best_overtime)) + 1, best_overtime)
-    plt.draw()
-    plt.pause(0.0001)
->>>>>>> 72debf778cab3d43425f2606e0f1fdbac5082207
     plt.clf()
